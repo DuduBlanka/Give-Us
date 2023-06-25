17816eb0b113156eeb2eff0e9815b76c1f2e38a1
Zohar Barel <58631735+zohar86@users.noreply.github.com>
2023-06-25 14:52:31 +0300

    Add files via upload

---
+++ runToProject/GiveUs- RunProject.cmd
@@ -0,0 +1,19 @@
+﻿@echo off
+
+REM Change the directory to the "server" folder
+cd server
+
+REM Start the server
+start npm start
+
+REM Wait for 5 seconds
+timeout /t 5
+
+REM Navigate out of the "server" folder
+cd..
+
+REM Change the directory to the "client/general" folder
+cd client\general
+
+REM Open the HTML file
+start "" HomePage.html
